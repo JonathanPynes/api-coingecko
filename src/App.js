473e687507508fe289e844e09cd7f6d4f807fe2b
Jonathan Pynes <jonathanpynes@Jonathans-MBP.winston>
2021-12-05 18:26:50 -0600

    cool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+
+  const [adaPrice, setAdaPrice] = useState({});
+  const [input, setInput] = useState("");
+  const [dollars, setDollars] = useState("0.00");
+
+  const calculatePrice = (e) => {
+    e.persist();
+    setInput(e.target.value)
+    console.log(dollars)
+  };
+
+async function fetchPrice() {
+    const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd");
+    res
+      .json()
+      .then(res => setAdaPrice(res))
+      .then(console.log(adaPrice))
+}
+
+useEffect(() => {
+  fetchPrice()
+}, [])
+
+useEffect(() => {
+    console.log("Input inside UseEffect: ", input, adaPrice)
+    if (adaPrice.cardano?.usd) {
+setDollars(adaPrice.cardano.usd * input)
+}
+
+    
+  }, [input]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a super simple app to practice fetching data from coingecko and using it to calculate price.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <span>Price of Cardano in ${dollars}</span>
+         <input
+         name="item"
+         type="number"
+         placeholder="Add ADA"
+         value={input}
+         onChange={(e) => calculatePrice(e)}
+         ></input>
+        
       </header>
+      <span>${dollars}</span>
     </div>
   );
 }
